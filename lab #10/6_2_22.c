137624f1092af51e0b2ce382f6c4b076c986e2cd
paulina0127 <73948761+paulina0127@users.noreply.github.com>
2021-05-28 02:09:59 +0200

    Zmiany w 6_2_22.c

--- lab #10/6_2_22.c
+++ lab #10/6_2_22.c
@@ -1,49 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void odwroc(int **tab, int n, int m) {
+void odwroc(int n, int m, int tab[][m]) {
     int temp = 0;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m/2; j++) {
-            temp = (*(*(tab+i)+j));
-            *(*(tab+i)+j) = *(*(tab+i)+(m-1-j));
-            *(*(tab+i)+(m-1-j)) = temp;
+            temp = tab[i][j];
+            tab[i][j] = tab[i][m-1-j];
+            tab[i][m-1-j] = temp;
         }
     }
 }
 
-int **alokuj(int n, int m) {
-    int **tab = malloc(n*sizeof(int *));
-
-    for (int i = 0; i < n; i++) {
-        *(tab+i) = malloc(m*sizeof(int));
-    }
-
-    return tab;
-}
-
-void pokaz(int **tab, int n, int m) {
+void pokaz(int n, int m, int tab[][m]) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            printf("[%d, %d] - %d\n", i, j, *(*(tab+i)+j));
+            printf("[%d, %d] - %d\n", i, j, tab[i][j]);
         }
     }
 }
 
 int main() {
-    int w = 2, k = 4;
-    int **tab = alokuj(w,k);
+    int w = 2, k = 3;
+    int tab[w][k];
 
     for (int i = 0; i < w; i++) {
         for (int j = 0; j < k; j++) {
-            *(*(tab+i)+j) = i+j;
-            printf("[%d, %d] - %d\n", i, j, *(*(tab+i)+j));
+            tab[i][j] = i+j;
+            printf("[%d, %d] - %d\n", i, j, tab[i][j]);
         }
     }
 
-    odwroc(tab,w,k);
+    odwroc(w,k,tab);
     printf("\nPo zamianie:\n");
-    pokaz(tab,w,k);
+    pokaz(w,k,tab);
     return 0;
 }
